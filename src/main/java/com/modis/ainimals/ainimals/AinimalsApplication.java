613207ce76f0cf9b4836d85a11b7f3656abc4816
Dorsk <mourajulien@gmail.com>
2022-07-04 16:37:15 +0200

    first commit - spring boot

---
+++ src/main/java/com/modis/ainimals/ainimals/AinimalsApplication.java
@@ -0,0 +1,13 @@
+package com.modis.ainimals.ainimals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AinimalsApplication.class, args);
+	}
+
+}
