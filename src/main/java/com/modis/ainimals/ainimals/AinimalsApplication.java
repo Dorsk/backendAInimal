5e729546f1568b3ab32bc64710e46f7524c5d87e
Dorsk <mourajulien@gmail.com>
2022-07-05 16:23:21 +0200

    new feature : upload 1 image

--- src/main/java/com/modis/ainimals/ainimals/AinimalsApplication.java
+++ src/main/java/com/modis/ainimals/ainimals/AinimalsApplication.java
@@ -2,8 +2,10 @@ package com.modis.ainimals.ainimals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.modis.ainimals.ainimals"})
 public class AinimalsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/modis/ainimals/ainimals/load/ImagesAndLabelsController.java
+++ src/main/java/com/modis/ainimals/ainimals/load/ImagesAndLabelsController.java
@@ -1,30 +1,42 @@
 package com.modis.ainimals.ainimals.load;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.ServletContext;
+
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.modis.ainimals.ainimals.utils.FileUploadUtils;
 
 @RestController
+@ComponentScan({"com.modis.ainimals.ainimals.load"})
 public class ImagesAndLabelsController {
 
 	private final AtomicLong counter = new AtomicLong();
 
+	@Autowired
+	ServletContext context;
 	/**
-	 * Home page 
 	 * récupération des labels 
+	 * 
 	 * @param label
 	 * @return
 	 */
@@ -39,10 +51,12 @@ public class ImagesAndLabelsController {
 	 * Home page 
 	 * récupération des images
 	 * @param label
+	 * @return 
 	 * @return
 	 */
-	@RequestMapping(value = "/upload", method = RequestMethod.GET)
-	public void uploadImage(@RequestParam("image") MultipartFile multipartFile) {
+//	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	@PostMapping("/upload")
+	public RedirectView uploadImage(@RequestParam("image") MultipartFile multipartFile) {
 		
 		// recuperation des images
 		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
@@ -50,9 +64,10 @@ public class ImagesAndLabelsController {
 		// recuperation de la date actuelle pour faire un dossier pour les images qui sont upload
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
-        String uploadDir = "user-photos/" + dateFormat.format(cal.getTime()).replace(" ", "_") + "/upload/";
- 
+        String uploadDir = context.getRealPath("user-photos");
+        uploadDir = uploadDir + File.separator + dateFormat.format(cal.getTime()).replace("/", "-").replace(" ", "_").replace(":", "-") + File.separator + "upload" + File.separator;
         try {
+        	System.out.println("Path new dir : " + uploadDir);
 			FileUploadUtils.saveFile(uploadDir, fileName, multipartFile);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -60,7 +75,7 @@ public class ImagesAndLabelsController {
 		}
                 
          // redirection vers une autre page web
-//        return new RedirectView("/upload", true);
+        return new RedirectView("/", true);
 	}
 	
 }
