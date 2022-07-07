9c8ba063d98e97ee9016f0aecec1bdd232a53427
Dorsk <mourajulien@gmail.com>
2022-07-07 11:35:56 +0200

    create file txt that contain lables

--- src/main/java/com/modis/ainimals/ainimals/upload/ImagesAndLabelsController.java
+++ src/main/java/com/modis/ainimals/ainimals/upload/ImagesAndLabelsController.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.ServletContext;
@@ -62,6 +64,29 @@ public class ImagesAndLabelsController {
 			@RequestParam("label7") String sLabel7, @RequestParam("label8") String sLabel8, @RequestParam("label9") String sLabel9,
 			@RequestParam("label10") String sLabel10) {
 		
+		// check labels utilisables
+		List <String> listLabels = new ArrayList<>();
+    	if(sLabel1!= null && !sLabel1.isEmpty() && !sLabel1.isBlank())
+    		listLabels.add(sLabel1);
+    	if(sLabel2!= null && !sLabel2.isEmpty() && !sLabel2.isBlank())
+    		listLabels.add(sLabel2);
+    	if(sLabel3!= null && !sLabel3.isEmpty() && !sLabel3.isBlank())
+    		listLabels.add(sLabel3);
+    	if(sLabel4!= null && !sLabel4.isEmpty() && !sLabel4.isBlank())
+    		listLabels.add(sLabel4);
+    	if(sLabel5!= null && !sLabel5.isEmpty() && !sLabel5.isBlank())
+    		listLabels.add(sLabel5);
+    	if(sLabel6!= null && !sLabel6.isEmpty() && !sLabel6.isBlank())
+    		listLabels.add(sLabel6);
+    	if(sLabel7!= null && !sLabel7.isEmpty() && !sLabel7.isBlank())
+    		listLabels.add(sLabel7);
+    	if(sLabel8!= null && !sLabel8.isEmpty() && !sLabel8.isBlank())
+    		listLabels.add(sLabel8);
+    	if(sLabel9!= null && !sLabel9.isEmpty() && !sLabel9.isBlank())
+    		listLabels.add(sLabel9);
+    	if(sLabel10!= null && !sLabel10.isEmpty() && !sLabel10.isBlank())
+    		listLabels.add(sLabel10);
+    	
 		// bloquage si 0 image à upload
 		if(multipartFiles.length > 1 || (multipartFiles.length == 1 && !multipartFiles[0].getOriginalFilename().equals(""))) {
 			// recuperation de la date actuelle pour faire un dossier pour les images qui sont upload
@@ -69,22 +94,29 @@ public class ImagesAndLabelsController {
 			Calendar cal = Calendar.getInstance();
 	        String uploadDir = context.getRealPath("user-photos");
 	        uploadDir = uploadDir + File.separator + dateFormat.format(cal.getTime()).replace("/", "-").replace(" ", "_").replace(":", "-") + File.separator + "upload" + File.separator;
-			
+	        System.out.println("-- Path Dir ----- : " + uploadDir);
+	       
 	        // recuperation des images et sauvegarder dans un dossier de partage
 			for (MultipartFile multipartFile : multipartFiles) {
 				String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-		       
 		        try {
-		        	System.out.println("-- Path Dir ----- : " + uploadDir);
-		        	System.out.println("-- File name ---- : " + fileName);
+		        	System.out.println("-- Image name ---- : " + fileName);
 					FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 				} catch (IOException e) {
 					// TODO LOGGER
 					e.printStackTrace();
 				}
-			}   
+			} 
 			
-			// lancer le script python
+			// creer fichier txt des labels
+			try {
+				FileUploadUtil.saveLabelsFile(uploadDir, "labels.txt", listLabels);
+				System.out.println("-- File name ---- : labels.txt");
+			} catch (IOException e) {
+				// TODO LOGGER
+				e.printStackTrace();
+			}
+			// TODO  lancer le script python
 			
 		}
 		 // redirection vers une autre page web
--- src/main/java/com/modis/ainimals/ainimals/utils/FileUploadUtil.java
+++ src/main/java/com/modis/ainimals/ainimals/utils/FileUploadUtil.java
@@ -1,11 +1,14 @@
 package com.modis.ainimals.ainimals.utils;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -34,5 +37,20 @@ public class FileUploadUtil {
             throw new IOException("Could not save image file: " + fileName, ioe);
         }      
     }
+
+	public static void saveLabelsFile(String uploadDir, String sFileName, List<String> listLabels) throws IOException {
+		 
+		Path uploadPath = Paths.get(uploadDir);
+		 
+		if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+		FileWriter fw = new FileWriter(uploadDir + File.separator + sFileName);
+		 
+		for (String label : listLabels) {
+			fw.write(label);
+		}
+		fw.close();
+	}
 	
 }
